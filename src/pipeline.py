ee479e4e8c1babcdbd2f46771973f71266957260
Karan Choudhary <kchoudhary510199@gmail.com>
2024-10-13 16:00:42 +0530

    Updated refactor code logic with partial inputs

--- src/pipeline.py
+++ src/pipeline.py
@@ -14,17 +14,49 @@ class Pipeline(CommandRecognizer):
         self.recognizer = sr.Recognizer()
         self.mic = sr.Microphone()
         self.voice = 'en-AU-NatashaNeural'
+        
+    def refactor_additional_details(self):
+        speak('Please Provide variable current name: ')
+        voice_dict = recognize_speech_from_mic(self.recognizer, self.mic)
+        current_name = voice_dict['alternative'][0]['transcript']
+        # current_name = str(input("Enter old name: ")).lower()
+        print(f"{current_name=}")
+        speak('Please Provide variable new name: ')
+        voice_dict = recognize_speech_from_mic(self.recognizer, self.mic)
+        new_name = voice_dict['alternative'][0]['transcript']
+        # new_name = str(input("Enter new name: ")).lower()
+        print(f"{new_name=}")
+        speak('Do you want to change all files?')
+        voice_dict = recognize_speech_from_mic(self.recognizer, self.mic)
+        all_files = voice_dict['alternative'][0]['transcript']
+        # all_files = str(input("yes or no: ")).lower()
+        if all_files.lower().strip() != 'yes':
+            speak('Please provide file names: ')
+            voice_dict = recognize_speech_from_mic(self.recognizer, self.mic)
+            file_name = voice_dict['alternative'][0]['transcript']
+            # file_name = str(input("Enter command: ")).lower()
+            print(f"{file_name=}")
+            all_files = file_name
+        return {
+            'old_name': current_name,
+            'new_name': new_name,
+            'file_name': all_files,
+        }
 
     def get_matching_command(self, said_command):
         command_idx, command_label = self.predict(said_command)
         print(f"{command_idx=}")
         print(f"{command_label=}")
+        if command_idx == 7:
+            additional_data = self.refactor_additional_details()
+        else:
+            additional_data = {}
         func_to_run = DYNAMIC_FUNC_MAPPINGS[command_idx]
-        return func_to_run(said_text=said_command)
+        return func_to_run(said_text=said_command, additional_data=additional_data)
     
     def execute_command(self, command):
         os.system(command)
-        # print(f"{command=}")
+        print(f"{command=}")
         speak('command successfully executed!')
         return
 
@@ -36,11 +68,11 @@ class Pipeline(CommandRecognizer):
         if "wake up" in said_command or "hey" in said_command:
             speak('I am ready')
         while True:
-            if said_command == 'exit':
-                exit()
+            # said_command = str(input("Enter command: ")).lower()
             voice_dict = recognize_speech_from_mic(self.recognizer, self.mic)
             said_command = voice_dict['alternative'][0]['transcript']
-            # said_command = str(input("Enter command: ")).lower()
+            if said_command == 'exit':
+                exit()
             print(f"{said_command=}")
             command = self.get_matching_command(said_command)
             self.execute_command(command)
--- utils/commands.py
+++ utils/commands.py
@@ -15,7 +15,7 @@ class Commands:
         text = re.sub(r'\s+', ' ', text)
         return text
     
-    def touch_command(self, said_text):
+    def touch_command(self, said_text, additional_data):
         partial_command = "touch"
         file_name = said_text.split('create')[1].strip()
         file_name = file_name.replace('dot', '.').lower()
@@ -23,22 +23,22 @@ class Commands:
         file_name = re.sub(r'\s+', '', file_name)
         return f"{partial_command} {file_name}"
     
-    def mkdir_command(self, said_text):
+    def mkdir_command(self, said_text, additional_data):
         partial_command = 'mkdir'
         dir_name = said_text.split('directory')[1].strip()
         return f"{partial_command} {dir_name}"
     
-    def pkiill_command(self, said_text):
+    def pkiill_command(self, said_text, additional_data):
         partial_command = 'pkill -f'
         process_name = said_text.split('kill')[1].strip()
         return f"{partial_command} {process_name}"
     
-    def rm_command(self, said_text):
+    def rm_command(self, said_text, additional_data):
         partial_command = 'rm -r'
         directory_name = said_text.split('delete')[1].strip()
         return f"{partial_command} {directory_name}"
     
-    def show_stats(self, said_text):
+    def show_stats(self, said_text, additional_data):
         if "btop" in said_text.lower() or "b top" in said_text.lower():
             return "btop"
         elif "htop" in said_text.lower() or "h top" in said_text.lower():
@@ -46,22 +46,30 @@ class Commands:
         else:
             return "s-tui"
     
-    def create_ml_template(self, said_text):
+    def create_ml_template(self, said_text, additional_data):
         project_name = said_text.split('project')[1].strip().lower()
         project_name = self._preprocess_text(project_name)
         start_project(project_name, 'ml_project')
         return "echo 'DONE'"
     
-    def general_project_template(self, said_text):
+    def general_project_template(self, said_text, additional_data):
         project_name = said_text.split('project')[1].strip().lower()
         project_name = self._preprocess_text(project_name)
         start_project(project_name, 'general_project')
         return "echo 'DONE'"
     
-    def refactor_code(self, said_text):
-        current_name = said_text.split('old name')[1].split('new name')[0].strip().title()
-        new_name = said_text.split('new name')[1].strip().title()
-        return f'''
-        sed -i 's/{current_name}/{new_name}/g' $(grep -rl --include='*.py' --exclude-dir={{.asst_env,.doc_env,src}} '{current_name}' .)
-        '''
-        
+    def refactor_code(self, said_text, additional_data):
+        old_name = additional_data.get('old_name')
+        new_name = additional_data.get('new_name')
+        assert old_name is not None and new_name is not None, "old_name and new_name cannot be None"
+        old_name = old_name.title()
+        new_name = new_name.title()
+        file_names = additional_data.get('file_name')
+        partial_command = f'''
+        sed -i 's/{old_name}/{new_name}/g' $(grep -rl '''
+        if file_names is None or len(file_names)==0 or file_names.lower()=='yes':
+            partial_command = f"{partial_command}--include='*.py'"
+            end_part = '.'
+        else:
+            end_part = f"{file_names}"
+        return f"{partial_command} --exclude-dir={{.asst_env,.doc_env,src}} '{old_name}' {end_part})"
